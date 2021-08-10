eae5ccdc12f3fc84ee1c3cbb5f707e8e32cc2874
dicook <visnut@gmail.com>
2021-08-10 10:17:01 +1000

    small fixes to langauge

--- inst/sample-article/sample-article.R
+++ inst/sample-article/sample-article.R
@@ -7,8 +7,10 @@ library(palmerpenguins)
 
 ## ----penguins-plotly, echo = TRUE, fig.height = 5, fig.cap="A basic interactive plot made with the plotly package on palmer penguin data. Three species of penguins are plotted with bill depth on the x-axis and bill length on the y-axis. When hovering on a point, a tooltip will show the exact value of the bill depth and length for that point, along with the species name."----
 p <- penguins %>% 
+  filter(species != "Gentoo") %>%
   ggplot(aes(x = bill_depth_mm, y = bill_length_mm, 
              color = species)) + 
+  scale_colour_brewer("", palette = "Dark2") +
   geom_point()
 ggplotly(p)
 
