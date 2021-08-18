73232be4d6d010f6cbf5c9d109ff25362458dced
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-08-18 11:00:05 +1000

    Conditionally output html/pdf plot for sample-article

--- inst/sample-article/sample-article.R
+++ inst/sample-article/sample-article.R
@@ -5,12 +5,19 @@ library(ggplot2)
 library(palmerpenguins)
 
 
-## ----penguins-plotly, echo = TRUE, fig.height = 5, fig.cap="A basic interactive plot made with the plotly package on palmer penguin data. Three species of penguins are plotted with bill depth on the x-axis and bill length on the y-axis. When hovering on a point, a tooltip will show the exact value of the bill depth and length for that point, along with the species name."----
-p <- penguins %>% 
+## ----penguins-plotly, echo = TRUE, fig.height = 5, fig.cap="A basic interactive plot made with the plotly package on palmer penguin data. Three species of penguins are plotted with bill depth on the x-axis and bill length on the y-axis. When hovering on a point, a tooltip will show the exact value of the bill depth and length for that point, along with the species name.", include=knitr::is_html_output(), eval=knitr::is_html_output()----
+#> p <- penguins %>%
+#>   ggplot(aes(x = bill_depth_mm, y = bill_length_mm,
+#>              color = species)) +
+#>   geom_point()
+#> ggplotly(p)
+
+
+## ----penguins-ggplot, echo = TRUE, fig.height = 5, fig.cap="A basic non-interactive plot made with the ggplot2 package on palmer penguin data. Three species of penguins are plotted with bill depth on the x-axis and bill length on the y-axis. Visit the online article to access the interactive version made with the plotly package.", include=knitr::is_latex_output(), eval=knitr::is_latex_output()----
+penguins %>% 
   ggplot(aes(x = bill_depth_mm, y = bill_length_mm, 
              color = species)) + 
   geom_point()
-ggplotly(p)
 
 ```{.r .distill-force-highlighting-css}
 ```
