8676d1763d63411eb0f7fb7aa2ebbc9af86d4d83
wkumler <wkumler@uw.edu>
2021-10-12 12:08:31 -0700

    Adding NA handling for check_abstract_before_intro to avoid workflow error in initial_check_article when intro or abstract aren't found

--- R/check.R
+++ R/check.R
@@ -236,11 +236,17 @@ check_abstract_before_intro <- function(path){
   abstract <- abstract[!is.na(abstract)][1]
   intro <- stringr::str_locate(tex, "introduction")[,"start"]
   intro <- intro[!is.na(intro)][1]
-
-  if (abstract < intro){
-    log_success("Abstract comes before the introduction section")
-  } else{
+  
+  if(is.na(abstract)){
+    log_error("Unable to find abstract! Please check for the \abstract tag",
+              " in your Tex document")
+  } else if(is.na(intro)){
+    log_error("Unable to find introduction! Please check for an intro in", 
+              "your Tex document")
+  } else if (abstract > intro){
     log_error("Abstract doesn't come before the introduction section")
+  } else {
+    log_success("Abstract comes before the introduction section")
   }
 }
 
