6e849fbd4f8eaf74e9c42922eab3b0713f6e5b2f
Sherry Zhang <huizezhangsh@gmail.com>
2021-12-02 11:12:29 +1100

    align outputs from `create_article` with articles created from rmd template

--- R/rjournal_article.R
+++ R/rjournal_article.R
@@ -8,35 +8,24 @@
 #' @importFrom stringr str_extract
 #' @importFrom fs dir_create file_copy file_move
 #' @export
-create_article <- function(file_name = "article", dir_path = here::here()){
+create_article <- function(file_name = "article", dir_path = "paper"){
 
+  force(dir_path)
   # read in all the template files
   folder <- c("skeleton", "resources")
   templates <- fs::dir_ls(system.file("rmarkdown", "templates", "rjournal", folder, package = "rjtools"))
 
   # create directory
   fs::dir_create(path = dir_path)
-  fs::dir_create(path = file.path(dir_path, "figures/"))
 
   # move rmd as well as change the bib parameter as file_name
   template_rmd <- xfun::read_utf8(templates[str_detect(templates, "Rmd")])
-  template_rmd <- whisker::whisker.render(
-    template = template_rmd,
-    data = list(bibfile = xfun::with_ext(file_name, "bib")))
-  template_rmd <- str_split(template_rmd, "\n")[[1]]
-  xfun::write_utf8(
-    file.path(dir_path, xfun::with_ext(file_name, "Rmd")),
-    template_rmd, quiet = TRUE)
+  xfun::write_utf8(template_rmd,
+                   file.path(dir_path, xfun::with_ext(file_name, "Rmd")))
 
   # move all others templates
   all_others <- templates[!str_detect(templates, "Rmd")]
   fs::file_copy(all_others, dir_path, overwrite = TRUE)
-
-  # rename/re-move bib file and penguins png
-  fs::file_move(file.path(dir_path, "RJreferences.bib"),
-                file.path(dir_path, xfun::with_ext(file_name, "bib")))
-  fs::file_move(file.path(dir_path, "penguins.png"),
-                file.path(dir_path, "figures", "penguins.png"))
 
 
   cli::cli_alert_success("Article created :)")
