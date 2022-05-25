6c7306cde4eb56a75f8ffd548c71224d0cccd741
mitchelloharawild <mitch.ohara-wild@monash.edu>
2022-05-26 01:28:31 +1000

    Add single issue PDF to rjournal_web_issue

--- R/rj_web_issue.R
+++ R/rj_web_issue.R
@@ -24,9 +24,9 @@ rjournal_web_issue <- function(toc = FALSE, self_contained = FALSE, ...) {
     input <- xfun::read_utf8(input_file)
     front_matter_delimiters <- grep("^(---|\\.\\.\\.)\\s*$", input)
 
+    issue_slug <- paste("RJ", metadata$volume + 2008, metadata$issue, sep = "-")
+
     issue_articles <- list_issue_articles(metadata$volume, metadata$issue)
-    issue_article_sort <- as.Date(vapply(issue_articles, date_received_from_metadata, character(1L)))
-    issue_articles <- issue_articles[order(issue_article_sort)]
     article_slugs <- vapply(issue_articles, function(art) art[["slug"]], character(1L))
 
     special_slugs <- unlist(metadata$articles, use.names = FALSE)
@@ -75,12 +75,17 @@ rjournal_web_issue <- function(toc = FALSE, self_contained = FALSE, ...) {
     )
     news <- issue_news[match(metadata$news, news_slugs)]
 
+    svg_pdf <- '<svg viewBox="0 0 384 512" style="height:1em;position:relative;display:inline-block;top:.1em;" xmlns="http://www.w3.org/2000/svg">
+      <path d="M181.9 256.1c-5-16-4.9-46.9-2-46.9 8.4 0 7.6 36.9 2 46.9zm-1.7 47.2c-7.7 20.2-17.3 43.3-28.4 62.7 18.3-7 39-17.2 62.9-21.9-12.7-9.6-24.9-23.4-34.5-40.8zM86.1 428.1c0 .8 13.2-5.4 34.9-40.2-6.7 6.3-29.1 24.5-34.9 40.2zM248 160h136v328c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V24C0 10.7 10.7 0 24 0h200v136c0 13.2 10.8 24 24 24zm-8 171.8c-20-12.2-33.3-29-42.7-53.8 4.5-18.5 11.6-46.6 6.2-64.2-4.7-29.4-42.4-26.5-47.8-6.8-5 18.3-.4 44.1 8.1 77-11.6 27.6-28.7 64.6-40.8 85.8-.1 0-.1.1-.2.1-27.1 13.9-73.6 44.5-54.5 68 5.6 6.9 16 10 21.5 10 17.9 0 35.7-18 61.1-61.8 25.8-8.5 54.1-19.1 79-23.2 21.7 11.8 47.1 19.5 64 19.5 29.2 0 31.2-32 19.7-43.4-13.9-13.6-54.3-9.7-73.6-7.2zM377 105L279 7c-4.5-4.5-10.6-7-17-7h-6v128h128v-6.1c0-6.3-2.5-12.4-7-16.9zm-74.1 255.3c4.1-2.7-2.5-11.9-42.8-9 37.1 15.8 42.8 9 42.8 9z"></path>
+      </svg>'
     xfun::write_utf8(
       c(
         "---",
         yaml::as.yaml(metadata),
         "---",
         input[(front_matter_delimiters[2]+1):length(input)],
+        "",
+        paste0("[Complete issue ", svg_pdf, "](", xfun::with_ext(issue_slug, "pdf"), ")"),
         "",
         "## Table of contents",
         news_entries(editorial),
@@ -137,6 +142,10 @@ list_issue_articles <- function(volume, issue) {
     function(yml) yml$volume == volume && yml$issue == issue,
     front_matter
   )
+
+  # Sort by received date
+  issue_article_sort <- as.Date(vapply(issue_articles, date_received_from_metadata, character(1L)))
+  issue_articles[order(issue_article_sort)]
 }
 
 list_issue_news <- function(volume, issue) {
@@ -190,7 +199,6 @@ article_entry <- function(art) {
   if(!is.character(art$author)) {
     art$author <- vapply(art$author, function(z) z$name, character(1L))
   }
-
 
   stringr::str_glue(
     "[{art$title}](../../articles/{art$slug})<br>{glue::glue_collapse(art$author, sep = ', ', last = ' and ')} {art$journal$firstpage %||% art$pages[1]}\n\n"
