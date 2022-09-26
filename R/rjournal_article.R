726a42e0b0154f918f89ab2d08099e459b488f7d
mitchelloharawild <mitch.ohara-wild@monash.edu>
2022-09-26 14:20:02 +1000

    Rewrite create_article() to use rmarkdown::draft()

--- R/rjournal_article.R
+++ R/rjournal_article.R
@@ -3,36 +3,45 @@
 #' @details
 #' See \code{vignette("create_article", package = "rjtools")} for how to create an article
 #'
-#' @param file_name the name of the Rmd file
-#' @param dir_path the directory name that houses the template files
+#' @param name the name of the Rmd
+#' @inheritParams rmarkdown::draft
+#'
 #' @return a created R Journal template in the directory specified by `dir_path`
+#'
 #' @importFrom stringr str_extract
 #' @importFrom fs dir_create file_copy file_move
+#'
 #' @examples
 #' \dontrun{
-#' create_article(file_name = "quokka-bilby", dir_path = "rjarticle")
+#' create_article(name = "quokka-bilby")
 #' }
+#'
 #' @export
-create_article <- function(file_name = "quokka-bilby", dir_path = "rjarticle"){
+create_article <- function(name, file = xfun::with_ext(name, "Rmd"), create_dir = TRUE, edit = TRUE){
+  rmarkdown::draft(
+    file,
+    template = "rjournal",
+    package = "rjtools",
+    create_dir = create_dir,
+    edit = FALSE
+  )
 
-  force(dir_path)
-  # read in all the template files
-  folder <- c("skeleton", "resources")
-  templates <- fs::dir_ls(system.file("rmarkdown", "templates", "rjournal", folder, package = "rjtools"))
-
-  # create directory
-  fs::dir_create(path = dir_path)
-
-  # move rmd as well as change the bib parameter as file_name
-  template_rmd <- xfun::read_utf8(templates[str_detect(templates, "Rmd")])
-  xfun::write_utf8(template_rmd,
-                   file.path(dir_path, xfun::with_ext(file_name, "Rmd")))
-
-  # move all others templates
-  all_others <- templates[!str_detect(templates, "Rmd")]
-  fs::file_copy(all_others, dir_path, overwrite = TRUE)
-
-
-  cli::cli_alert_success(glue::glue("Article created in the directory called ", dir_path, " :)"))
-
+  if(edit) {
+    path <- if(create_dir) {
+      file.path(xfun::sans_ext(file), file)
+    } else {
+      file
+    }
+    path <- normalizePath(path)
+    has_rstudio <- if(requireNamespace("rstudioapi", quietly = TRUE)) {
+      rstudioapi::hasFun("navigateToFile")
+    } else {
+      FALSE
+    }
+    if(has_rstudio) {
+      rstudioapi::navigateToFile(path)
+    } else {
+      utils::file.edit(path)
+    }
+  }
 }
