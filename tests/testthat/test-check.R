aadbd91565b64143ca8e23762ebeddfb03e62ac2
Simon Urbanek <simon.urbanek@R-project.org>
2023-01-06 20:38:39 +1300

    update tests

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,3 +1,7 @@
+## some tests require CRAN access - make sure a mirror is set
+if ("@CRAN@" %in% getOption("repos"))
+    chooseCRANmirror(FALSE, 1, TRUE)
+
 test_that("different styles for titles are ok", {
   # check_str is the function used in check_title
 
@@ -26,36 +30,63 @@ test_that("different styles for titles are ok", {
   expect_true(rjtools:::check_str(str, ignore = c("toOoOlTiPs", "data.table"))$result)
 })
 
+expect_SUCCESS <- function(expr) expect_equal(c(expr), "SUCCESS")
+expect_NOTE <- function(expr) expect_equal(c(expr), "NOTE")
+expect_ERROR <- function(expr) expect_equal(c(expr), "ERROR")
+
+article_path <- system.file("sample-article", package = "rjtools")
+bad_article_path <- "../bad-article"
+
+#test_that("filenames check works", {
+#    expect_SUCCESS(check_filenames(article_path))
+#})
+
+test_that("structure check works", {
+    expect_SUCCESS(check_structure(article_path))
+    ## we cannot ship a file with spaces since that would trigger
+    ## check failure, so we have to create it here
+    writeLines("foo", fws <- file.path(bad_article_path, "file with spaces"))
+    expect_ERROR(check_structure(bad_article_path))
+    unlink(fws)
+    ## create invalid nesting
+    dir.create(file.path(bad_article_path, "a", "b", "c", "d"), TRUE, TRUE)
+    expect_ERROR(check_structure(bad_article_path))
+    unlink(file.path(bad_article_path, "a"), TRUE)
+    ## create unicode file name
+    writeLines("foo", fwu <- file.path(bad_article_path, "\u010cesko"))
+    expect_ERROR(check_structure(bad_article_path))
+    unlink(fwu)
+    expect_SUCCESS(check_structure(bad_article_path))
+})
+
 test_that("title check works", {
-  article_path <- system.file("sample-article", package = "rjtools")
-  expect_true(is.null(check_title(article_path)))
+    expect_SUCCESS(check_title(article_path))
+    expect_ERROR(check_title(bad_article_path))
 })
 
 test_that("section check works", {
-  article_path <- system.file("sample-article", package = "rjtools")
-  expect_true(is.null(check_section(article_path)))
+    expect_SUCCESS(check_section(article_path))
 })
 
 test_that("cover letter check works", {
-  article_path <- system.file("sample-article", package = "rjtools")
-  expect_true(is.null(check_cover_letter(article_path)))
+    expect_SUCCESS(check_cover_letter(article_path))
+    expect_NOTE(check_cover_letter(bad_article_path))
 })
 
 test_that("unnecessary check works", {
-  article_path <- system.file("sample-article", package = "rjtools")
-  expect_true(is.null(check_unnecessary_files(article_path)))
+    expect_SUCCESS(check_unnecessary_files(article_path))
+    expect_ERROR(check_unnecessary_files(bad_article_path))
 })
 
 test_that("spelling check works", {
-  article_path <- system.file("sample-article", package = "rjtools")
-  expect_true(is.null(check_spelling(article_path)))
+    expect_NOTE(check_spelling(article_path))
 })
 
 test_that("proposed package check works", {
-  expect_true(is.null(check_proposed_pkg("ggplot2")))
+    expect_SUCCESS(check_proposed_pkg("ggplot2"))
+    expect_NOTE(check_proposed_pkg("nonexistent"))
 })
 
-#test_that("available packages check works", {
-#  article_path <- system.file("sample-article", package = "rjtools")
-#  expect_true(is.null(check_packages_available(article_path, ignore=c("ToOoOlTiPs"))))
-#})
+test_that("available packages check works", {
+    expect_SUCCESS(check_packages_available(article_path, ignore="ToOoOlTiPs"))
+})
