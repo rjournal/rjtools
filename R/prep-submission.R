2e732c14803e10e5c691833d22292323e7cdf6d2
huizezhang-sherry <huizezhangsh@gmail.com>
2023-10-05 16:15:06 +1100

    rewrite zip_paper() since now there is check_folder_structure() #106

--- R/prep-submission.R
+++ R/prep-submission.R
@@ -1,80 +1,38 @@
 #' Zip your directory for R Journal submission
 #'
-#' Zip up  files in the directory into a \code{paper.zip} for R Journal submission.
+#' \code{zip_paper} will first check the folder structure with
+#' [rjtools::check_folder_structure()] before zipping up everything in the
+#' main directory, excluding the `.Rproj` file, if exist.
 #'
-#'
-#' @param name the file name you used to create the article. See argument `file_name` in \code{create_article()}
-#' @param others other files or folders to zip, this might include supplementary \code{data/} folder,
-#' motivational letter for add-in packages, and \code{review/} folder for reviewer's comments and responses.
 #' @importFrom utils zip
-#' @importFrom yesno yesno
-#' @return a zip file with items for an R Journal submission
+#' @importFrom yesno yesno2
+#' @return a zip file for an R Journal submission
 #' @export
 #'
-zip_paper <- function(name, others = NULL){
-
-
-  # check whether the paper folder already exist
-  if (fs::dir_exists("paper") & !fs::file_exists("paper.zip")){
-  delete_paper <- yesno::yesno(
-  "Paper folder should be reserved for the zipped file and Rjtools is going to delete it.
-  Do you want to proceed?")
-  if (delete_paper) {
-    fs::dir_delete("paper")
-  } else{
-    cli::cli_abort("Terminated by the user.")
-  }
-
-}
+zip_paper <- function(){
 
   # remove the paper.zip and its associated paper/ if users have unzipped before
   # regenerate a new zip
   if (fs::file_exists("paper.zip")) {
-    cli::cli_inform("Removing created paper.zip and associated paper folder if detected.")
+    cli::cli_inform("Removing the {.file paper.zip} file detected.")
     fs::file_delete("paper.zip")
     if (fs::dir_exists("paper")) fs::dir_delete("paper")
-
   }
 
-  # should find .Rmd, .tex, .bib, .html from the dual rendering,
-  # and .r for the script if applicable
-  basic <- list.files(pattern = name, recursive = TRUE, full.names = TRUE)
-  html_files <- list.dirs(path = file.path(dirname(basic[1]), glue::glue("{name}_files")))
-
-  # should find RJwrapper.tex
-  rjwrapper <- list.files(pattern = "RJwrapper",
-                          recursive = TRUE, full.names = TRUE)
-
-  # should find style sheet
-  sty <- list.files(pattern = "Rjournal.sty",
-                          recursive = TRUE, full.names = TRUE)
-
-  motivation_letter <- list.files(pattern = "motivation-letter.md",
-                    recursive = TRUE, full.names = TRUE)
-
-  # process other files to zip
-  file_path <- others[fs::path_ext(others) != ""]
-  folder_path <- others[fs::path_ext(others) == ""]
-
-  # detect whether the claimed files and folders exist
-  test_folders <- purrr::map_lgl(folder_path, fs::dir_exists)
-  test_files <- purrr::map_lgl(file_path, fs::file_exists)
-  if (!all(test_folders) & length(file_path) != 0){
-    cli::cli_abort("Folder {.code {folders[!test_folder]}} {?is/are} not detected.")
-  }
-  if (!all(test_files) & length(file_path) != 0){
-    cli::cli_abort("File {.code {files[!test_files]}} {?is/are} not detected in the top level directory.")
+  cli::cli_h1("Check for folder structure")
+  res <- check_folder_structure(".")
+  cli::cli_h1("Prepare paper zip")
+  if (res != "SUCCESS"){
+    continue <- yesno::yesno2(
+    "ERRORs or WARNINGs generated from checking the folder structure.
+    Please fix before creating the paper zip. Would you like to continue?")
   }
 
-  other_paths <- unique(c(file_path, folder_path))
-
-  # cli::cli_inform("Copy and paste the following to the supplementary file in the submission form:
-  #                 ")
-
-  # collect everything and zip!
-  to_zip <- c(basic, html_files, rjwrapper, sty, motivation_letter, other_paths)
-  utils::zip(zipfile = "paper", to_zip)
-
+  if (continue | res == "SUCCESS"){
+    files <- list.files(recursive = TRUE, full.names = TRUE)
+    files <- files[!tools::file_ext(files) %in% "Rproj"]
+    utils::zip(zipfile = "paper", files)
+  }
 
 }
 
